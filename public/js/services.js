5605079a8a1bc4e6da467c26e92ad83a112fc059
xiaolei <tristones.liu@gmail.com>
2013-06-12 23:18:48 +0800

    add call-to-action

--- public/js/services.js
+++ public/js/services.js
@@ -5,5 +5,5 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('ten20.services', []).
+var app = angular.module('ten20.services', []).
   value('version', '0.1');
