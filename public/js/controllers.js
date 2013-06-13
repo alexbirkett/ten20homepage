3bfc7a86149c6504af974632136b0f7d48806b7c
xiaolei <tristones.liu@gmail.com>
2013-06-13 10:43:29 +0800

    change polyline style & adding twinking effects

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -29,7 +29,7 @@ function FrontPageCtrl($scope, $http, $window) {
 
       for (var i = 0; i < mapParam.circles.length; i++) {
         var circle= $scope.window.L.circle(mapParam.circles[i], 100, circleOption).addTo(map);
-        map.addLayer(circle);
+        map.addLayer(circle); 
       };
 
       // add polyline
@@ -37,21 +37,47 @@ function FrontPageCtrl($scope, $http, $window) {
         stroke: true,
         weight: 2,
         color: '#f60',
+        dashArray: '3,4'
       };
-      var index = 0;
       var polyLine = $scope.window.L.polyline([], polyOption).addTo(map);
 
+      var point = $scope.window.L.circle(mapParam.polyLine[0], 100, circleOption).addTo(map);
+      twinkleCircle(point);
+
+      // twinkling effects for polyline start point
+      function twinkleCircle(circle) {
+        var radius = [100, 130, 160, 190, 220, 230, 220, 210, 
+                      200,190, 180, 170, 160, 150, 140, 130, 120, 110];
+        var index = 0;
+        var times = 0;
+        var intervalId = $scope.window.setInterval(function() {
+            (index == (radius.length - 1))?(index = 0, times++):index++;
+            if (times == 2) {
+              $scope.window.clearInterval(intervalId); 
+              drawPolylines();
+              return;
+            }
+            circle.setRadius(radius[index]);
+        }, 100);
+      }
+
       // animate polylines
-      // TODO: change stlye to dotted
-      $scope.window.setInterval(function() {
-        index++;
-        if (index == mapParam.polyLine.length) {
-          polyLine.spliceLatLngs(0, index);
-          index = 0;
-          return;
-        }
-        polyLine.addLatLng(mapParam.polyLine[index]);
-      }, 700);
+      function drawPolylines() {
+        var index = 0;
+        var intervalId = $scope.window.setInterval(function() {
+          index++;
+          if (index == mapParam.polyLine.length) {
+            polyLine.spliceLatLngs(0, index);
+            index = 0;
+            $scope.window.clearInterval(intervalId); 
+            twinkleCircle(point);
+            return;
+          }
+          polyLine.addLatLng(mapParam.polyLine[index]);
+        }, 700);
+      }
+
+
 
     }).
     error(function(data, status, headers, config) {
