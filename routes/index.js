6b41a3add162a66e8e26cb2883dd64fdeacb8b76
Alex Birkett <alex@birkett.co.uk>
2013-06-19 12:27:35 +0200

    Load models based on url

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,28 @@ var model = require('../public/data/model.json');
 var partner = require('../public/data/partner.json');
 
 exports.index = function(req, res){
-  if (req.path == '/') {
-    res.render('index', model);
-  } else if (req.path == '/partner') {
-    res.render('index', partner);
-  } else {
-    res.render('404', {pageTitle: 'Page Not Found'});
-  }
+
+      var model;
+
+      if (req.path == '/') {
+          model =  require('../public/data/model.json');
+      } else {
+           // strip out any nasties like ..
+           var pattern =/([a-z_-]+)/i;
+           var output = pattern.exec(req.path);
+
+           try {
+               model = require('../public/data/' + output[1]);
+           } catch (e) {
+               // ignore
+           }
+      }
+
+      if (model) {
+          res.render('index', model);
+      } else {
+          res.render('404', {pageTitle: 'Page Not Found'});
+      }
 };
 
 exports.partials = function (req, res) {
