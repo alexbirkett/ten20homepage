ab4b32330a63250bee742d94c2304e2dd73428bd
xiaolei <tristones.liu@gmail.com>
2013-08-08 23:59:28 +0800

    build angular app, wrapping mapbox services and serving data using socket.io

--- public/scripts/angular/services.js
+++ public/scripts/angular/services.js
@@ -6,11 +6,17 @@
 // In this case it is a simple value service.
 angular.module('ten20Angular.services', []).
   factory('mapbox', function () {
-    // body...
-    return {
-      updateCurrent: function() {},
-      updateHistory: function() {},
-      toggleStreetView: function() {}
+    return function(latlng) {
+      var params = {
+        "lat": latlng[0],
+        "lng": latlng[1],
+        "zoomLevel":14,
+        "usermap": true,
+        "zoomControl":true,
+        "tile":"alexbirkett.map-t0fodlre",
+        "layers":[{"label":"Satellite","tileLayer":"alexbirkett.map-t0fodlre"},{"label":"Map","tileLayer":"alexbirkett.map-bugector"}]
+      };
+      return new ten20.MapRender('map', params);
     };
   }).
   value('version', '0.0.1');
