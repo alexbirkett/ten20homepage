881f541800329ab72e4cf82f1bd647e9febb3ae7
xiaolei <tristones.liu@gmail.com>
2014-01-13 18:24:38 +0800

    styling tracker setting modal dialog

--- public/scripts/angular/directives/modalForm.js
+++ public/scripts/angular/directives/modalForm.js
@@ -7,42 +7,60 @@ angular.module('ten20Angular').
     return {
 			restrict: 'A',
       scope: {
-        dataSrc: "=", // data model bind to the modal dialog template
+        srcModel: "=", // data model bind to the modal dialog template
         tplUrl: "@",  // modal dialog template url
         method: "@",  // server request method, POST, PUT, etc
-        path: "@"     // server request path
+        path: "@",   // server request path
+        dialogClass: "@"   // modal dialog width
       },
       controller: ['$scope', '$modal', function($scope, $modal) {
 
         $scope.open = function () {
 
-        var modalInstance = $modal.open({
-          templateUrl: $scope.tplUrl,
-          controller: ModalInstanceCtrl,
-          resolve: {
-            data: function () {
-              return $scope.dataSrc;
-            }
-          }
-        });
+          var modalInstance = $modal.open({
+            templateUrl: $scope.tplUrl,
+            controller: ModalInstanceCtrl,
+            resolve: {
+              config: function () {
+                return {
+                  src: $scope.srcModel,
+                  ajaxMethod: $scope.method,
+                  ajaxUrl: $scope.path,
+                  width: $scope.width
+                };
+              }
+            },
+            windowClass: $scope.dialogClass
+          });
 
-        modalInstance.result.then(function (data) {
-          $scope.submitData = data;
-        }, function () {
-        });
+          modalInstance.result.then(function (data) {
+            $scope.submitData = data;
+          }, function () {
+          });
         };
 
-        var ModalInstanceCtrl = function ($scope, $modalInstance, data) {
+        var ModalInstanceCtrl = function($scope, $http, $modalInstance, config) {
 
-          $scope.data = data;
+          $scope.data = config.src;
 
-          $scope.submit = function () {
-            $modalInstance.close($scope.data);
+          $scope.ok = function () {
+            $scope.showLoadingBar = true;
+            $http({
+              method: config.ajaxMethod,
+              url: config.ajaxUrl,
+              data: config.src
+            }).success(function () {
+              console.log('reqeust success');
+            }).error(function (argument) {
+              console.log('reqeust fail');
+            });
+            //$modalInstance.close($scope.data);
           };
 
           $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
           };
+
         };
 
       }],
