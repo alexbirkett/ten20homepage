27f376c4f1ae17a7e14ae052c279ed0d5b1a9445
xiaolei <tristones.liu@gmail.com>
2014-01-21 16:53:00 +0800

    fix map layer label style error

--- public/scripts/angular/directives/userMap.js
+++ public/scripts/angular/directives/userMap.js
@@ -79,12 +79,12 @@ angular.module('ten20Angular').
             "zoomControl":true,
             "layers":[
               {
-                "label":"Map",
-                "tileLayer":"alexbirkett.map-bugector"
-              },
-              {
                 "label":"Satellite",
                 "tileLayer":"alexbirkett.map-t0fodlre"
+              },
+              {
+                "label":"Map",
+                "tileLayer":"alexbirkett.map-bugector"
               }
             ]
           };
