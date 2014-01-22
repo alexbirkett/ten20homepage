d1e7e3bb838753e81f8f2d0bb91fe12b96c97211
xiaolei <tristones.liu@gmail.com>
2014-01-22 10:22:42 +0800

    move plans section to angular front

--- public/scripts/angular/controllers/home.js
+++ public/scripts/angular/controllers/home.js
@@ -27,18 +27,7 @@ angular.module('ten20Angular.controllers').
         method: "POST",
         path: "/signup",
         class: "small",
-        redirect: "/console",
-        data: {
-          free: {
-            type: "free"
-          },
-          family: {
-            type: "family"
-          },
-          enterprise: {
-            type: "enterprise"
-          }
-        }
+        redirect: "/console"
       }
       
     };
@@ -126,6 +115,48 @@ angular.module('ten20Angular.controllers').
           "icon": "trusted.png",
           "title": "Trusted",
           "text": "ten20 (Birkett Enterprise Ltd) has 6 years experience running GPS tracking service with 400,000 registered users."
+        }
+      ]
+    };
+
+    $scope.plans = {
+      "title": "- PLANS -",
+      "name": "plans",
+
+      "items": [
+        {
+          "name": "FIRST TRACKER",
+          "url": "#free-plan",
+          "id": "free",
+          "price": "0",
+          "data": {
+            "type": "free-plan"
+          },
+          "highlighted": false,
+          "features": ["Track one tracking device or phone for free", "Real time location", "Trip history" ]
+
+        },
+        {
+          "name": "FAMILY",
+          "url": "#family-plan",
+          "id": "family",
+          "price": "20",
+          "data": {
+            "type": "family-plan"
+          },
+          "highlighted": true,
+          "features": ["Our most popular plan", "Real time location", "Trip history", "Virtual fence", "Up to five trackers and phones on the map simultaneously", "Login for each family member", "Warm feeling that you're supporting ten20"]
+        },
+        {
+          "name": "ENTERPRISE",
+          "id": "enterprise",
+          "url": "#enterprise-plan",
+          "price": "50",
+          "data": {
+            "type": "enterprise-plan"
+          },
+          "highlighted": false,
+          "features": ["Great for fleet tracking", "Telephone support", "Real time location", "Trip history", "Virtual fence", "Unlimited number of trackers", "10 logins", "Warm feeling that you're supporting ten20"]
         }
       ]
     };
