d63515780dbfe282b07aa849759dcfb9f4e0f7a7
xiaolei <tristones.liu@gmail.com>
2014-01-23 12:25:13 +0800

    implement click on trackers

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -343,6 +343,17 @@
       marker = this.addPoint(latlng, opt);
       marker.trackerId = tracker._id;
 
+      marker.on('click', function() {
+        var panels = $('.tool-box .panel');
+
+        panels.each(function(index, panel) {
+          var scope = $(panel).scope();
+          if (scope.tracker._id === tracker._id) {
+            $(panel).find('.panel-heading a').click();
+          }
+        });
+      });
+
       this.map.addLayer(marker);
       this.markers.push(marker);
       this.map.panTo(marker.getLatLng());
