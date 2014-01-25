6866c241e801c00a973a1f05de3fcbb2deedec8d
xiaolei <tristones.liu@gmail.com>
2014-01-25 14:21:07 +0800

    add tests for root and partner controller

---
+++ test/spec/controllers/rootSpec.js
@@ -0,0 +1,28 @@
+/*global describe, it */
+'use strict';
+
+describe("Root controller test", function () {
+  var ctrl, scope, createController;
+
+  beforeEach(module('ten20Angular'));
+
+  beforeEach(inject(function ($rootScope, $controller) {
+
+    scope = $rootScope.$new();
+
+    createController = function() {
+      return $controller('RootCtrl', {'$scope' : scope});
+    };
+  }));
+
+  it("controller init correctly", function () {
+    createController();
+    expect(scope.form).toBeDefined();
+    expect(scope.form.contact).toBeDefined();
+    expect(scope.form.signin.path).toBe('/signin');
+    expect(scope.about).toBeDefined();
+    expect(scope.benefits).toBeDefined();
+    expect(scope.faq).toBeDefined();
+  });
+
+});
