5744a82fc64f27d9c4772dc964e4ebd8b31578b4
xiaolei <tristones.liu@gmail.com>
2014-02-08 09:05:36 +0800

    add carousel section and tweak styles

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -59,6 +59,55 @@ $(function(){
         }, 1000);
       }
     });
-  }
+  };
+
+  // init carousel
+  $('.jcarousel')
+    .on('jcarousel:create jcarousel:reload', function() {
+      var element = $(this),
+          width = element.innerWidth();
+
+      // This shows 1 item at a time.
+      // Divide `width` to the number of items you want to display,
+      // eg. `width = width / 3` to display 3 items at a time.
+      element.jcarousel('items').css('width', width + 'px');
+    }).jcarousel({
+      center: true,
+      wrap: 'circular',
+      transitions: true,
+      animation: {
+          duration: 400,
+          easing:   'linear',
+          complete: function() {
+          }
+      },
+    }).jcarouselAutoscroll({
+      interval: 3000,
+      target: '+=1',
+      autostart: true
+    });
+
+  $('.jcarousel-prev').jcarouselControl({
+    target: '-=1'
+  });
+
+  $('.jcarousel-next').jcarouselControl({
+    target: '+=1'
+  });
+
+  
+  $('.jcarousel-pagination').jcarouselPagination({
+    item: function(page) {
+      return '<a href="#' + page + '"></a>';
+    }
+  });
+
+  $('.jcarousel-pagination').on('jcarouselpagination:active', 'a', function() {
+    $(this).addClass('active');
+  });
+
+  $('.jcarousel-pagination').on('jcarouselpagination:inactive', 'a', function() {
+    $(this).removeClass('active');
+  });
 
 });
