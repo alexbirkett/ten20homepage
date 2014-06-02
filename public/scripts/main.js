610da473f9edcd74f1ffb63e7a82da8219a402bb
xiaolei <tristones.liu@gmail.com>
2014-06-02 16:32:19 +0800

    remove useless code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -17,50 +17,6 @@ $(function(){
     }
   });
 
-
-  window.ten20.submitForm = function (postUrl, redirectUrl) {
-
-    var self = $('.submit-form');
-
-    self.find('.btn').on('click', function(e) {
-      e.preventDefault();
-
-      var data = {};
-
-      self.find('input').each(function () {
-        var name = $(this).attr('id');
-        data[name] = $(this).val();
-      });
-
-      if (data['re-password'] &&
-        data['re-password'] != data['password']) {
-          self.find('input[id="password"]').val('');
-          self.find('input[id="re-password"]').val('');
-          doAlert('password not match!');
-          return;
-        }
-
-      $.post(postUrl || '/', data, function(res) {
-        window.location = redirectUrl || '/';
-      }).fail(function(res) {
-        var responseJSON = res.responseJSON;
-        if (responseJSON && responseJSON.message && responseJSON.message !== '') {
-          doAlert(res.message);
-        }
-      });
-
-      function doAlert(msg) {
-        var alerter = self.find('.alerter');
-        alerter.text(msg);
-        alerter.show();
-
-        setTimeout(function () {
-          alerter.fadeOut(2000);
-        }, 1000);
-      }
-    });
-  };
-
   // init carousel
   $('.jcarousel')
     .on('jcarousel:create jcarousel:reload', function() {
