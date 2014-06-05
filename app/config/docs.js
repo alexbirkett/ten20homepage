0399970b311f401a5547d303f261cd54476acec1
Alex Birkett <alex@birkett.co.uk>
2014-06-05 21:43:57 +0200

    Use same nav for docs and home

--- app/config/docs.js
+++ app/config/docs.js
@@ -1,28 +1,4 @@
 module.exports = {
   head: require('./sections/head'),
-  navs: [
-      {
-        "text": "Supported hardware",
-        "link": "/#supported",
-        "scroll": "true"
-      },
-      {
-        "link": "/console",
-        "text": "sign in"
-      },
-      {
-        "link": "/docs",
-        "text": "Documentation"
-      },
-      {
-          "text": "Subreddit forum",
-          "link": "http://www.reddit.com/r/ten20/",
-          "scroll": "false"
-      },
-      {
-        "text": "About Us",
-        "link": "/#about-us",
-        "scroll": "true"
-      }
-    ]
+  navs: require('./sections/nav')
 };
