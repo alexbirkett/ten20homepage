6b79d9046aa110875006da93128460bb2f9ff84f
Alex Birkett <alex@birkett.co.uk>
2014-06-05 21:43:57 +0200

    Use same nav for docs and home

---
+++ app/config/sections/nav.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        "text": "Supported hardware",
+        "link": "/#supported",
+        "scroll": "true"
+    },
+    {
+        "modal": true,
+        "id": "signup",
+        "text": "Sign up"
+    },
+    {
+        "link": "/console",
+        "text": "Sign in"
+    },
+    {
+        "link": "/docs",
+        "text": "Documentation"
+    },
+    {
+        "text": "Subreddit forum",
+        "link": "http://www.reddit.com/r/ten20/",
+        "scroll": "false"
+    },
+    {
+        "text": "About Us",
+        "link": "/#about-us",
+        "scroll": "true"
+    }
+]
